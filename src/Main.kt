6f4e590ee24c9c9af46d5bcecea08c3165a76a3e
10221234 <komarova10102006@gmail.com>
2024-09-17 20:42:18 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val valOne = 5
+    val valTwo = 4
+    val valSumma = valOne + valTwo
+    println("Сумма равна $valSumma")
+}
